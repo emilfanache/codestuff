1d7b442524e033ca2993f5219207f135269790eb
Emil Fanache <emilfanache@gmail.com>
2020-11-09 19:51:43 -0500

    Simple Linked List implementation

---
+++ C++/SingleLinkedList/SingleLinkedList.hpp
@@ -0,0 +1,40 @@
+/*
+ * SingleLinkedList.hpp
+ *
+ *  Created on: Nov. 7, 2020
+ *      Author: Emil Fanache
+ */
+
+#ifndef SINGLELINKEDLIST_HPP_
+#define SINGLELINKEDLIST_HPP_
+
+#include <string>
+
+using namespace std;
+
+typedef struct Node_t
+{
+	Node_t*   _next;
+	int       _data;
+} Node;
+
+
+class SingleLinkedList
+{
+private:
+	Node* head;
+	bool debug;
+
+public:
+	SingleLinkedList();
+	SingleLinkedList(bool dbg);
+	~SingleLinkedList();
+	void PrintList(string description);
+	void ReverseList();
+	void AddEndNode(int data);
+	void AddInsideNode(int dataToFind, int dataToAdd);
+	void DeleteNode(int data);
+};
+
+
+#endif /* SINGLELINKEDLIST_HPP_ */
---
+++ C++/SingleLinkedList/main.cpp
@@ -0,0 +1,30 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Nov. 9, 2020
+ *      Author: Emil Fanache
+ */
+
+#include "SingleLinkedList.hpp"
+
+
+int main()
+{
+	SingleLinkedList Slist(false);
+
+	Slist.AddEndNode(7);
+	Slist.AddEndNode(8);
+	Slist.AddEndNode(9);
+	Slist.AddEndNode(10);
+	Slist.AddEndNode(14);
+	Slist.PrintList("Initial creation by adding end nodes");
+	Slist.DeleteNode(9);
+	Slist.PrintList("After Node is being deleted");
+	Slist.AddInsideNode(10, 12);
+	Slist.AddInsideNode(14, 15);
+	Slist.PrintList("After adding inside nodes");
+	Slist.ReverseList();
+	Slist.PrintList("After reversing list");
+
+	return 0;
+}
