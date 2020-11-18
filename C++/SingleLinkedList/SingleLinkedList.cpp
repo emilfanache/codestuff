d29745c0711e550c8d078ba3bfbda8323f7e992f
Emil Fanache <emilfanache@gmail.com>
2020-11-17 22:59:43 -0500

    Fix indentation

--- C++/SingleLinkedList/SingleLinkedList.cpp
+++ C++/SingleLinkedList/SingleLinkedList.cpp
@@ -6,167 +6,167 @@ using namespace std;
 
 SingleLinkedList::SingleLinkedList()
 {
-	head = nullptr;
-	debug = false;
+    head = nullptr;
+    debug = false;
 }
 
 SingleLinkedList::SingleLinkedList(bool dbg)
 {
-	head = nullptr;
-	debug = dbg;
+    head = nullptr;
+    debug = dbg;
 }
 
 void SingleLinkedList::AddEndNode(int data)
 {
-	Node *n = new Node;
-	n->_data = data;
-	n->_next = nullptr;
+    Node *n = new Node;
+    n->_data = data;
+    n->_next = nullptr;
 
-	if (!head)
-	{
-		head = n;
-	}
-	else
-	{
-		Node *current;
+    if (!head)
+    {
+        head = n;
+    }
+    else
+    {
+        Node *current;
 
-		// this should take me to the end of the list
-		for (current = head; current->_next != nullptr; current = current->_next);
-		if (debug)
-		{
-			cout << "I'm at: [" << current->_data << "], adding node:"<< n->_data << endl;
-		}
-		current->_next = n;
-	}
+        // this should take me to the end of the list
+        for (current = head; current->_next != nullptr; current = current->_next);
+        if (debug)
+        {
+            cout << "I'm at: [" << current->_data << "], adding node:"<< n->_data << endl;
+        }
+        current->_next = n;
+    }
 
-	return;
+    return;
 }
 
 void SingleLinkedList::AddInsideNode(int dataToFind, int dataToAdd)
 {
-	if (!head)
-	{
-		AddEndNode(dataToAdd);
-	}
-	else
-	{
+    if (!head)
+    {
+        AddEndNode(dataToAdd);
+    }
+    else
+    {
 
-		Node *current;
-		bool foundData = false;
+        Node *current;
+        bool foundData = false;
 
-		for (current = head; current->_next != nullptr; current = current->_next)
-		{
-			if (current->_data == dataToFind)
-			{
-				foundData = true;
-				break;
-			}
-		}
+        for (current = head; current->_next != nullptr; current = current->_next)
+        {
+            if (current->_data == dataToFind)
+            {
+                foundData = true;
+                break;
+            }
+        }
 
-		if (!foundData || !current->_next)
-		{
-			if (debug)
-			{
-				cout << "AddInsideNode fallback: Adding end note" << endl;
-			}
-			AddEndNode(dataToAdd);
-		}
-		else
-		{
-			Node *n = new Node;
-			n->_data = dataToAdd;
-			n->_next = current->_next;
-			current->_next = n;
-		}
-	}
+        if (!foundData || !current->_next)
+        {
+            if (debug)
+            {
+                cout << "AddInsideNode fallback: Adding end note" << endl;
+            }
+            AddEndNode(dataToAdd);
+        }
+        else
+        {
+            Node *n = new Node;
+            n->_data = dataToAdd;
+            n->_next = current->_next;
+            current->_next = n;
+        }
+    }
 
-	return;
+    return;
 }
 
 void SingleLinkedList::DeleteNode(int data)
 {
-	if (!head)
-	{
-		return;
-	}
-	else
-	{
-		Node *current;
-		Node *previous;
-		bool foundData = false;
-		for (current = head; current->_next != nullptr; current = current->_next)
-		{
-			if (current->_data == data)
-			{
-				foundData = true;
-				break;
-			}
-			previous = current;
-		}
+    if (!head)
+    {
+        return;
+    }
+    else
+    {
+        Node *current;
+        Node *previous;
+        bool foundData = false;
+        for (current = head; current->_next != nullptr; current = current->_next)
+        {
+            if (current->_data == data)
+            {
+                foundData = true;
+                break;
+            }
+            previous = current;
+        }
 
-		if (foundData)
-		{
-			previous->_next = current->_next;
-			delete current;
-		}
-	}
+        if (foundData)
+        {
+            previous->_next = current->_next;
+            delete current;
+        }
+    }
 
-	return;
+    return;
 }
 
 void SingleLinkedList::ReverseList()
 {
-	Node *current = head;
-	Node *previous = nullptr;
-	Node *next = nullptr;
+    Node *current = head;
+    Node *previous = nullptr;
+    Node *next = nullptr;
 
-	while (current != nullptr)
-	{
-		next = current->_next;
-		current->_next = previous;
-		previous = current;
-		current = next;
-	}
+    while (current != nullptr)
+    {
+        next = current->_next;
+        current->_next = previous;
+        previous = current;
+        current = next;
+    }
 
-	head = previous;
+    head = previous;
 }
 
 void SingleLinkedList::PrintList(string description)
 {
-	if (!description.empty())
-	{
-		cout << "====== " << description.c_str() << " ======" << endl;
-	}
+    if (!description.empty())
+    {
+        cout << "====== " << description.c_str() << " ======" << endl;
+    }
 
-	for (Node * current = head; current != nullptr; current = current->_next)
-	{
-		cout << "[" << current->_data << "]";
-		if (current->_next != nullptr)
-		{
-			cout << "->";
-		}
-	}
+    for (Node * current = head; current != nullptr; current = current->_next)
+    {
+        cout << "[" << current->_data << "]";
+        if (current->_next != nullptr)
+        {
+            cout << "->";
+        }
+    }
 
-	cout << endl;
+    cout << endl;
 }
 
 SingleLinkedList::~SingleLinkedList()
 {
-	Node *next = nullptr;
-	Node *current = head;
+    Node *next = nullptr;
+    Node *current = head;
 
-	while (current != nullptr)
-	{
-		next = current->_next;
-		if (debug)
-		{
-			cout << "Destroying node with data:" << current->_data << endl;
-		}
-		delete current;
-		current = next;
-	}
+    while (current != nullptr)
+    {
+        next = current->_next;
+        if (debug)
+        {
+            cout << "Destroying node with data:" << current->_data << endl;
+        }
+        delete current;
+        current = next;
+    }
 
-	head = nullptr;
+    head = nullptr;
 }
 
 
