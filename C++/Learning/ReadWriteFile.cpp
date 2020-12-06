d3f26e480a165524ddd7d123ea975e51efadb805
emilfanache@gmail.com <emilfanache@gmail.com>
2020-12-05 22:52:36 -0500

    Add Read/Write to file example

---
+++ C++/Learning/ReadWriteFile.cpp
@@ -0,0 +1,86 @@
+/*
+ * ReadWriteFile.cpp
+ *
+ *  Created on: Dec. 5, 2020
+ *      Author: Emil Fanache
+ */
+
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+
+
+bool CharToRemove(unsigned char c)
+{
+    return (c == ' ' ||
+            c == '.' ||
+            c == ',' ||
+            c == ';');
+}
+
+std::string RemoveTraillingChars(std::string text)
+{
+    text.erase(std::remove_if(text.begin(),
+                              text.end(),
+                              CharToRemove),
+               text.end());
+
+    return text;
+}
+
+std::string BreakAndFormat(std::string inputText)
+{
+    std::string formatedStr;
+    std::istringstream ss(inputText);
+    std::string token;
+
+    while(std::getline(ss, token, ' ')) 
+    {
+        formatedStr += token + '#' + '\n';
+    }
+
+    return RemoveTraillingChars(formatedStr);
+}
+
+
+int main()
+{
+    std::string inputText;
+    std::ifstream inputFile("input_file.txt");
+    std::ofstream outputFile("output_file.txt");
+    int err = 0;
+
+    if (inputFile.is_open())
+    {
+        while (getline(inputFile, inputText))
+        {
+            if (outputFile.is_open())
+            {
+                std::string formated = BreakAndFormat(inputText);
+                outputFile << formated;
+            }
+            else
+            {
+                perror("Unable to open file: ");
+                err = 1;
+            }
+        }
+    }
+    else
+    {
+        perror("Unable to open file: ");
+        err = 1;
+    }
+
+    if (!err)
+    {
+        std::cout << "File written successfully!" << std::endl;
+    }
+    inputFile.close();
+    outputFile.close();
+
+    return 0;
+}
